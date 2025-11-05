2f70b1511d4d632ff1fa67b87946ee3f70a84702
backendStark <239644803+backendStark@users.noreply.github.com>
2025-11-05 10:01:02 +0500

    add new test to hello

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Dmitrii")
-	want := "Hello, Dmitrii"
+	t.Run("should type the name", func(t *testing.T) {
+		got := Hello("Dmitrii")
+		want := "Hello, Dmitrii"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("should type 'Hello, world' with epmty string", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
