e8ab3b60dcd0cb88fbab622002e7dda53b0339ae
backendStark <239644803+backendStark@users.noreply.github.com>
2025-11-05 10:01:53 +0500

    updated code for test

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func Hello(who string) string {
-	return fmt.Sprintf("Hello, %s", who)
+	if who == "" {
+		return "Hello, world"
+	} else {
+		return fmt.Sprintf("Hello, %s", who)
+	}
 }
 
 func main() {
