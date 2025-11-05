9cf1b620dbe55b39b589f9c3ca172a917afe263b
backendStark <239644803+backendStark@users.noreply.github.com>
2025-11-05 10:16:58 +0500

    updated code for french language

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -1,18 +1,23 @@
 package main
 
 const spanish = "Spanish"
+const french = "French"
 
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(who, language string) string {
 	if who == "" {
 		who = "world"
 	}
 
-	if language == spanish {
+	switch language {
+	case spanish:
 		return spanishHelloPrefix + who
-	} else {
+	case french:
+		return frenchHelloPrefix + who
+	default:
 		return englishHelloPrefix + who
 	}
 }
