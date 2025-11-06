324148c70a8733acb4973e14bb0f1d14d8d772d8
backendStark <239644803+backendStark@users.noreply.github.com>
2025-11-06 12:23:24 +0500

    add iterations test chapter

---
+++ iterations/iterations_test.go
@@ -0,0 +1,18 @@
+package iterations
+
+import "testing"
+
+// func TestRepeat(t *testing.T) {
+// 	got := Repeat("a", 5)
+// 	expected := "aaaaa"
+
+// 	if got != expected {
+// 		t.Errorf("expected %q but got %q", expected, got)
+// 	}
+// }
+
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a")
+	}
+}
