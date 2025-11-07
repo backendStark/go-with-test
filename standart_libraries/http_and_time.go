4e3adabd02a7b1946c700bf2bca1c61d043c5881
backendStark <239644803+backendStark@users.noreply.github.com>
2025-11-07 13:43:06 +0500

    add function to get IP addrs

--- standart_libraries/http_and_time.go
+++ standart_libraries/http_and_time.go
@@ -2,9 +2,33 @@ package main
 
 import (
 	"log"
+	"net"
 	"net/http"
+	"strings"
 	"time"
 )
+
+func clientIP(r *http.Request) string {
+	xff := r.Header.Get("X-Forwarded-For")
+	if xff != "" {
+		parts := strings.Split(xff, ",")
+		ip := strings.TrimSpace(parts[0])
+		if ip != "" {
+			return ip
+		}
+	}
+
+	if xrip := strings.TrimSpace(r.Header.Get("X-Real-IP")); xrip != "" {
+		return xrip
+	}
+
+	host, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err == nil && host != "" {
+		return host
+	}
+
+	return r.RemoteAddr
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
