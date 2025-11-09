509c3def8da89584ead35e791a0a4a1b75f0d7ca
backendStark <239644803+backendStark@users.noreply.github.com>
2025-11-09 17:04:54 +0500

    add simple test table unit test for ParseUserInput

--- fuzzing/fuzzing_test.go
+++ fuzzing/fuzzing_test.go
@@ -1,1 +1,36 @@
 package fuzzing
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_ParseUserInput_Success(t *testing.T) {
+	testCases := []struct {
+		input  string
+		isWork bool
+	}{
+		{"dmitrii:30", true},
+		{"raul :31", true},
+		{"nikita", false},
+		{"tanya:30:somethingnew", false},
+		{"lev:abv", false},
+		{"volodya:-45", false},
+		{"kostya:0", false},
+		{":100", false},
+		{":49", false},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("Test for string %q", tc.input), func(t *testing.T) {
+			res, err := ParseUserInput(tc.input)
+			if tc.isWork && err != nil {
+				t.Fatalf("should work, but got err %v", err)
+			}
+
+			if tc.isWork == false && err == nil {
+				t.Fatalf("should return error, but got result %v", res)
+			}
+		})
+	}
+}
